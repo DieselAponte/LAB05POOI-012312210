4f707e72942301d762f2dfa682dd41f8fd578b39
DieselAponte <143888820+DieselAponte@users.noreply.github.com>
2023-10-10 00:02:07 -0500

    Add files via upload

---
+++ Estudiante.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exposición;
+
+/**
+ *
+ * @author diese
+ */
+public class Estudiante extends Persona {
+    private String programa;
+    
+    public Estudiante(String nombre, int edad) {
+        super(nombre, edad);
+        this.programa = programa;
+    }
+    
+}
