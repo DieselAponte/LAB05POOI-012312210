9fa9c539808cc19a04239ebc8b19b9637d5c14e5
DieselAponte <143888820+DieselAponte@users.noreply.github.com>
2023-10-10 00:33:08 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exposición;
+
+/**
+ *
+ * @author diese
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Estudiante estudiante1 = new Estudiante("Jairo", 18, "Ingles");
+        
+        Profesor profesor1 = new Profesor("Cumpa Valencia");
+    }
+}
